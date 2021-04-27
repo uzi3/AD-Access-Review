c059ce8821c560094546d7a684edb01b88e08c1b
uzi3 <43525327+uzi3@users.noreply.github.com>
2021-04-27 18:32:56 +0530

    first commit

---
+++ AD access review.ps1
@@ -0,0 +1,35 @@
+﻿Class Perm
+{
+$name
+$OU
+$ACType
+$identityReference
+$ADRights
+$IsInhereted
+}
+
+Import-Module ActiveDirectory
+Set-Location AD:
+$OUs = @("DC=company,DC=pri","OU=Domain Controllers,DC=company,DC=pri","OU=Test OU,DC=company,DC=pri")
+foreach ($OU in $OUs)
+{
+
+$OU -match "\w\w="
+$Name = (($OU -split ",")[0]).replace($Matches.values,"")
+
+$ACLs = ((get-acl (Get-ADObject "$OU").distinguishedname).access) | select *
+
+    foreach ($ACL in $ACLs)
+    {
+    $obj = New-Object Perm
+
+    $obj.name = $Name
+    $obj.ou = $OU
+    $obj.ACType = $ACL.AccessControlType
+    $obj.identityReference = $ACL.IdentityReference
+    $obj.ADRights = $ACL.ActiveDirectoryRights
+    $obj.IsInhereted = $ACL.IsInherited
+
+    $obj | Export-Csv C:\temp\Access.csv -Append -NoTypeInformation
+    }
+}
